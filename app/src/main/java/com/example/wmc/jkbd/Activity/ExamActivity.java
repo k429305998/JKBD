cd3749e3638cdc291a3e7a5a6bcb3a06ddbb0a10
k429305998 <31767815@qq.com>
2017-06-30 01:03:24 +0800

    添加考试界面

---
+++ app/src/main/java/com/example/wmc/jkbd/Activity/ExamActivity.java
@@ -0,0 +1,19 @@
+package com.example.wmc.jkbd.Activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.wmc.jkbd.R;
+
+/**
+ * Created by Link on 2017/6/30.
+ */
+
+public class ExamActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exam);
+    }
+}
