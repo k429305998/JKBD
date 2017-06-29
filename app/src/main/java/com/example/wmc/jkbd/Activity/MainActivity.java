37287885391a93a05ca66acb5b47ae59a51f4df4
k429305998 <31767815@qq.com>
2017-06-30 01:11:00 +0800

    添加跳转及退出

--- app/src/main/java/com/example/wmc/jkbd/Activity/MainActivity.java
+++ app/src/main/java/com/example/wmc/jkbd/Activity/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.wmc.jkbd.Activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.example.wmc.jkbd.R;
@@ -15,5 +16,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void test(View view) {
+        startActivity(new Intent(MainActivity.this,ExamActivity.class));
+    }
+    public void exit(View view) {
+        finish();
     }
 }
