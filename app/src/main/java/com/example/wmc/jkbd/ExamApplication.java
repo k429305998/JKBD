cdbea50fc8025e4a42a8118cf0e08f77e9b5fdac
k429305998 <31767815@qq.com>
2017-07-02 20:48:47 +0800

    下载并显示考试题

--- app/src/main/java/com/example/wmc/jkbd/ExamApplication.java
+++ app/src/main/java/com/example/wmc/jkbd/ExamApplication.java
@@ -3,9 +3,11 @@ package com.example.wmc.jkbd;
 import android.app.Application;
 import android.util.Log;
 
-import com.example.wmc.jkbd.bean.Question;
 import com.example.wmc.jkbd.bean.Examination;
+import com.example.wmc.jkbd.bean.Question;
+import com.example.wmc.jkbd.bean.resource;
 import com.example.wmc.jkbd.utils.OkHttpUtils;
+import com.example.wmc.jkbd.utils.ResultUtils;
 
 import java.util.List;
 
@@ -16,30 +18,80 @@ import java.util.List;
 public class ExamApplication extends Application {
     Examination mExamInfo;
     List<Question> mExamList;
-    ExamApplication instance;
+    private static ExamApplication instance;
+
     @Override
     public void onCreate() {
         super.onCreate();
         instance = this;
+
         initData();
     }
 
-    private void initData() {
-        OkHttpUtils<Examination> utils = new OkHttpUtils<>(instance);
-        String uri = "http://101.251.196.90:8080/JztkServer/examInfo";
-        utils.url(uri)
-                .targetClass(Examination.class)
-                .execute(new OkHttpUtils.OnCompleteListener<Examination>() {
-                    @Override
-                    public void onSuccess(Examination result) {
-                        Log.e("main","result="+result);
-                        mExamInfo=result;
-                    }
+    public static ExamApplication getInstance(){
+        return instance;
+    }
 
-                    @Override
-                    public void onError(String error) {
-                        Log.e("main","error="+error);
-                    }
-                });
+    private void initData() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                OkHttpUtils<Examination> utils = new OkHttpUtils<>(instance);
+                String uri = "http://101.251.196.90:8080/JztkServer/examInfo";
+                utils.url(uri)
+                        .targetClass(Examination.class)
+                        .execute(new OkHttpUtils.OnCompleteListener<Examination>() {
+                            @Override
+                            public void onSuccess(Examination result) {
+                                Log.e("main","result="+result);
+                                mExamInfo = result;
+                            }
+
+                            @Override
+                            public void onError(String error) {
+                                Log.e("main","error="+error);
+                            }
+                        });
+
+
+                OkHttpUtils<String> utils1 = new OkHttpUtils<>(instance);
+                String url2 = "http://101.251.196.90:8080/JztkServer/getQuestions?testType=rand";
+                utils1.url(url2)
+                        .targetClass(String.class)
+                        .execute(new OkHttpUtils.OnCompleteListener<String>() {
+                            @Override
+                            public void onSuccess(String jsonStr) {
+                                resource result = ResultUtils.getListResultFromJson(jsonStr);
+                                if (result!=null && result.getError_code()==0){
+                                    List<Question> list = result.getResult();
+                                    if (list!=null && list.size()>0){
+                                        mExamList = list;
+                                    }
+                                }
+                            }
+
+                            @Override
+                            public void onError(String error) {
+                                Log.e("main","error="+error);
+                            }
+                        });
+            }
+        }).start();
+    }
+
+    public Examination getExamInfo() {
+        return mExamInfo;
+    }
+
+    public void setExamInfo(Examination examInfo) {
+        mExamInfo = examInfo;
+    }
+
+    public List<Question> getExamList() {
+        return mExamList;
+    }
+
+    public void setExamList(List<Question> examList) {
+        mExamList = examList;
     }
 }
