0baafef047f79c2dbfc3eb2fb0cefffec7889888
k429305998 <31767815@qq.com>
2017-07-02 21:45:13 +0800

    添加biz和dao包

---
+++ app/src/main/java/com/example/wmc/jkbd/biz/IExamBiz.java
@@ -0,0 +1,12 @@
+package com.example.wmc.jkbd.biz;
+
+/**
+ * Created by Link on 2017/7/2.
+ */
+
+public interface IExamBiz {
+    void beginExam();
+    void nextQuestion();
+    void preQuestion();
+    void commitExam();
+}
