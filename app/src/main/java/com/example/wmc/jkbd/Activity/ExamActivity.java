0331464284edc60c5cac3344000a1c9dd460552f
k429305998 <31767815@qq.com>
2017-07-04 20:56:11 +0800

    保存并显示答案

--- app/src/main/java/com/example/wmc/jkbd/Activity/ExamActivity.java
+++ app/src/main/java/com/example/wmc/jkbd/Activity/ExamActivity.java
@@ -178,13 +178,28 @@ public class ExamActivity extends AppCompatActivity {
             }else{
                 mImageView.setVisibility(View.GONE);
             }
-                       resetOptions();
-                    }
-           }
+            resetOptions();
+            String userAnswer = exam.getUserAnswer();
+             if (userAnswer!=null && !userAnswer.equals(""))
+             {
+                 int userCB = Integer.parseInt(userAnswer) - 1;
+                 cbs[userCB].setChecked(true);
+             }
+        }
+    }
 
-     private void resetOptions() {
-                        for (CheckBox cb : cbs) {
-                        cb.setChecked(false);
+    private void resetOptions() {
+        for (CheckBox cb : cbs) {
+            cb.setChecked(false);
+        }
+    }
+
+    private void saveUserAnswer(){
+        for (int i = 0; i < cbs.length; i++) {
+            if (cbs[i].isChecked()){
+                biz.getExam().setUserAnswer(String.valueOf(i+1));
+                return;
+            }
         }
     }
 
@@ -204,10 +219,12 @@ public class ExamActivity extends AppCompatActivity {
     }
 
     public void preExam(View view) {
+        saveUserAnswer();
         showExam(biz.preQuestion());
     }
 
     public void nextExam(View view) {
+        saveUserAnswer();
         showExam(biz.nextQuestion());
     }
 
