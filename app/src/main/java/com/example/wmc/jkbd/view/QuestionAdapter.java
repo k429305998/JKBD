844391f0f72aef2f74b282a3477ea368de83e9ab
k429305998 <31767815@qq.com>
2017-07-05 21:37:33 +0800

    保存答案并修改Gallery内图标

--- app/src/main/java/com/example/wmc/jkbd/view/QuestionAdapter.java
+++ app/src/main/java/com/example/wmc/jkbd/view/QuestionAdapter.java
@@ -1,6 +1,7 @@
 package com.example.wmc.jkbd.view;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -47,6 +48,13 @@ public class QuestionAdapter extends BaseAdapter {
         TextView tvNo = (TextView) view.findViewById(R.id.tv_no);
         ImageView ivQuestion = (ImageView) view.findViewById(R.id.iv_question);
         tvNo.setText("第"+(position+1)+"题");
+        String ua = examList.get(position).getUserAnswer();
+        Log.e("adapter","examList.get(position)="+examList.get(position));
+        if (ua!=null&& !ua.equals("")){
+        ivQuestion.setImageResource(R.mipmap.answer24x24);
+        }else{
+        ivQuestion.setImageResource(R.mipmap.ques24x24);
+        }
         return view;
     }
 }
